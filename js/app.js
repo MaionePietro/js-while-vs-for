fc9429b38432a10b7c9736ec4103630e0b52edbc
MaionePietro <pietromaionespam@gmail.com>
2023-02-07 22:20:37 +0100

    FEAT console log array

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 let arrayDispari = [];
 let input = 0;
 for (let i = 0; i < 10; i++) {
-    while(input%3==0){
-        input = prompt("inserisci numero");
+    while((input%2==0)){
+        input = parseInt(prompt("inserisci numero"));
     }
     arrayDispari.push(input);
 }
-console.log(arrayDispari);
+
+for(let i=0 ; i<10 ; i++){
+    console.log('nella cella di memoria: '+i+' e salvata la seguente cifra: '+arrayDispari[i]);
+}
